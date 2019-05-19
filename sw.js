02517b7f44ee0180a229a5aad0ef4b20b2f570c5
anibalsolon <anibalsolon@gmail.com>
2019-05-19 19:44:35 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-eeaff81539ea1cf58502.js"
+    "url": "webpack-runtime-4b54cef82f481005dd52.js"
   },
   {
     "url": "app-14dc4c2b50dff9712201.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "06fd1013c99a9410ffc7aeecbb8bd7c6"
+    "revision": "eb977ea47d701417a221538cf756239f"
   },
   {
     "url": "styles.b3ebce1a1a39de8cd78a.css"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
     "url": "1-79e20d213bc4cee6244e.js"
   },
   {
-    "url": "2-c7131583e17aa5ee27f0.js"
+    "url": "2-f4c12a9e05c80f1f2cbc.js"
   },
   {
     "url": "component---src-pages-404-jsx-b3565a80616af0aa407a.js"
