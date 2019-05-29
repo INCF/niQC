a9e53bf73bbb30d74155c932de99ac697ffe6e5e
Pradeep Reddy Raamana <raamana@gmail.com>
2019-05-29 23:11:06 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1af629f9d3f0496c9117.js"
+    "url": "webpack-runtime-961a929332a64fa404a3.js"
   },
   {
     "url": "app-7f2b9ee19bf99472dca2.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "624ec209efeecfe5d1b81397c12141d1"
+    "revision": "b4524a16a56bc6582caafbfef28c92d3"
   },
   {
     "url": "styles.b3ebce1a1a39de8cd78a.css"
